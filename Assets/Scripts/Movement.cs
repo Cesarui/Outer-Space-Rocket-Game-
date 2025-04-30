417ced06c0c98d7e3e83d543499e3ecebe0723b9
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-04-29 22:07:46 -0400

    Added some rocket movement and booster sound

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction boost;
+    [SerializeField] InputAction rotation;
+    [SerializeField] AudioClip boostSound;
+
+    [SerializeField] float boostStrenght = 5f;
+    [SerializeField] float rotationStrenght = 5f;
+
+    AudioSource audioSource;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
+    {
+        boost.Enable();
+        rotation.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        if (boost.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.forward * boostStrenght * Time.fixedDeltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(boostSound);
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+            // less than 0 is when the button is not being pressed and greater means its being pressed.
+
+            float rotationValue = rotation.ReadValue<float>();
+        if (rotationValue < 0)
+        {
+            rb.transform.Rotate(Vector3.left * rotationStrenght);
+        }
+        else if (rotationValue > 0)
+        {
+            rb.transform.Rotate(Vector3.right * rotationStrenght);
+        }
+    }
+
+}
